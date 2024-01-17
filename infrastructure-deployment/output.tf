2e1e0168ed3f5a3969bdd4d9976ab2523e6a1b64
randomstr1ng <julian@petersohn.it>
2024-01-17 11:35:14 +0100

    add deployment of second s4hana system

--- infrastructure-deployment/output.tf
+++ infrastructure-deployment/output.tf
@@ -17,9 +17,17 @@ output "admin-server-public-ip" {
 # Output IP Address of S4HANA Server
 output "s4hana-server-1-private-ip" {
     description = "value of the private IP address of the S4HANA server"
-    value = module.sap-s4hana.private_ip
+    value = module.sap-s4hana-1.private_ip
 }
 output "s4hana-server-1-public-ip" {
     description = "value of the public IP address of the S4HANA server"
     value = aws_eip.s4hana-1_eip.public_ip
+}
+output "s4hana-server-2-private-ip" {
+    description = "value of the private IP address of the S4HANA server"
+    value = module.sap-s4hana-2.private_ip
+}
+output "s4hana-server-2-public-ip" {
+    description = "value of the public IP address of the S4HANA server"
+    value = aws_eip.s4hana-2_eip.public_ip
 }
--- infrastructure-deployment/s4-hana.tf
+++ infrastructure-deployment/s4-hana.tf
@@ -1,4 +1,4 @@
-module "sap-s4hana" {
+module "sap-s4hana-1" {
   source = "./modules/s4-hana"
   security_group_name = "${var.s4_hana_security_group_name}-1"
   ec2_name            = "${var.s4_hana_ec2_name}-1"
@@ -8,5 +8,18 @@ module "sap-s4hana" {
   instance_type       = var.s4_hana_instance_type
 }
 resource "aws_eip" "s4hana-1_eip" {
-  instance = module.sap-s4hana.s4-hana_instance_id
+  instance = module.sap-s4hana-1.s4-hana_instance_id
+}
+
+module "sap-s4hana-2" {
+  source = "./modules/s4-hana"
+  security_group_name = "${var.s4_hana_security_group_name}-1"
+  ec2_name            = "${var.s4_hana_ec2_name}-1"
+  vpc-id              = aws_vpc.vpc.id
+  keypair-id          = aws_key_pair.key_pair.id
+  subnet-id           = aws_subnet.subnet.id
+  instance_type       = var.s4_hana_instance_type
+}
+resource "aws_eip" "s4hana-2_eip" {
+  instance = module.sap-s4hana-2.s4-hana_instance_id
 }
