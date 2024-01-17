b2a2c9f9f7d89eb5b5ad069dc44b8951e096c182
randomstr1ng <julian@petersohn.it>
2024-01-17 10:14:11 +0100

    add assignment of eip to s4hana instance

--- infrastructure-deployment/s4-hana.tf
+++ infrastructure-deployment/s4-hana.tf
@@ -1,14 +1,12 @@
 module "sap-s4hana" {
   source = "./modules/s4-hana"
-  count = 1
-  security_group_name = "${var.s4_hana_security_group_name}-${count.index}"
-  ec2_name            = "${var.s4_hana_ec2_name}-${count.index}"
+  security_group_name = "${var.s4_hana_security_group_name}-1"
+  ec2_name            = "${var.s4_hana_ec2_name}-1"
   vpc-id              = aws_vpc.vpc.id
   keypair-id          = aws_key_pair.key_pair.id
   subnet-id           = aws_subnet.subnet.id
   instance_type       = var.s4_hana_instance_type
 }
-resource "aws_eip" "s4hana_eip" {
-  for_each = toset(module.sap-s4hana.s4-hana_instance_id)
-  instance = module.sap-s4hana.[each.value]
+resource "aws_eip" "s4hana-1_eip" {
+  instance = module.sap-s4hana.s4-hana_instance_id
 }
